576646b576d832d34b5598ace789379b64833f40
Vasile-ovidiu DRUTA <vdruta@e1p57.cluj.42.fr>
2015-10-26 18:03:42 +0100

    workshop

---
+++ workshops/code-best-practices/psfcwnhoete.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_students_from_cluj_with_note_higher          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/26 16:46:29 by vdruta            #+#    #+#             */
+/*   Updated: 2015/10/26 17:51:43 by vdruta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int		main()
+{
+	FILE *fp;
+	char buff[255];
+	char a[20],b[20],c[20],d[5],e[10];
+	int i=0,j=0,k=0;
+
+	fp = fopen("students.csv", "r");
+	while(fscanf(fp, "%s", buff) > 0)
+	{
+		j=0;k=0;i=0;
+		while(*(buff+i) != 0)
+		{
+			if(buff[i] == ';')
+			{
+				j++;
+				k=0;
+			}
+			else{
+				if (j == 0)
+				{
+					a[k] = buff[i];
+					if(buff[i + 1] == ';')
+						a[k + 1] = '\0';
+				}
+				if(j==1)
+				{
+				b[k] = buff[i];
+				if(buff[i+1]== ';')
+					b[k+1]='\0';
+				}
+				if(j==2)
+				{
+				c[k] = buff[i];
+				if(buff[i+1]== ';')
+					c[k+1]='\0';
+				}
+				if(j==3)
+				{
+				d[k] = buff[i];
+				if(buff[i+1]== ';')
+					d[k+1]='\0';
+				}
+				if(j==4)
+				{
+				e[k] = buff[i];
+				if(buff[i+1]== 0)
+					e[k+1]='\0';
+				}
+				k++;
+			}
+			i++;
+		}
+	
+		if(strcmp(e,"Cluj") == 0 && atof(d) > 8.00)
+		{
+			printf("%s %s\n",a,b);
+		}
+	}
+	return (0);
+}
