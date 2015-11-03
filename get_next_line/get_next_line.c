2ec7079b47312d191b14f09be83af0b25cfa9186
Vasile-ovidiu DRUTA <vdruta@e1p55.cluj.42.fr>
2015-11-03 13:36:54 +0100

    gnl

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/03 13:05:13 by vdruta            #+#    #+#             */
+/*   Updated: 2015/11/03 13:35:51 by vdruta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		get_next_line(int const fd, char ** line)
+{
+	
+}
+
+int		main(void)
+{
+	int		fd;
+	int		ret;
+	char	buff[BUFF_SIZE + 1];
+
+	if (!(fd = open("file1", O_RDONLY)))
+		return (0);
+	while (ret = read(fd, buff, BUFF_SIZE))
+	{
+		buff[ret] = '\0';
+	}
+	return (0);
+}
