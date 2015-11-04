db349f2ca7b8bcab200aa81f34551b0ff937acbb
Vasile-ovidiu DRUTA <vdruta@e1p18.cluj.42.fr>
2015-11-04 13:21:51 +0100

    jos ponta

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,27 +6,47 @@
 /*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/03 13:05:13 by vdruta            #+#    #+#             */
-/*   Updated: 2015/11/03 14:51:43 by vdruta           ###   ########.fr       */
+/*   Updated: 2015/11/04 13:20:59 by vdruta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		get_next_line(int const fd, char ** line)
+#include "get_next_line.h"
+
+int		get_next_line(int const fd, char **line)
 {
+	static char	*buff = NULL;
+	char		*pos;
+	int			ret;
 	
+	if (BUFF_SIZE < 1 || !line)
+		return (-1);
+	if (!(buff = (char*)malloc(sizeof(*buff) * (BUFF_SIZE + 1))))
+		return (-1);
+	pos = ft_strchr(buff, '\n');
+	if (pos == NULL)
+	{
+		ret = ft_read_fd_to_stock(fd, &stock);
+		if (ret < 0)
+			return (-1);
+		if (ret == 0)
+	}
+	else if (pos != NULL)
+	{
+		
+	}
+
 }
 
 int		main(void)
 {
 	int		fd;
 	int		ret;
-	char	buff[BUFF_SIZE + 1];
+	char	buff[BUFF_SIZE];
 
 	fd = open("file1", O_RDONLY);
 	if (fd == -1)
-		return (1);
+		return (0);
 	while (ret = read(fd, buff, BUFF_SIZE))
-	{
-		get_next_line(fd, &buff);
-	}
+		get_next_line(fd, buff);
 	return (0);
 }
