f61de3853d2ea388ea27215c7875fefd80ea793b
Vasile-ovidiu DRUTA <vdruta@e1p74.cluj.42.fr>
2015-11-06 15:08:38 +0100

    gnl done

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,111 +6,58 @@
 /*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/03 13:05:13 by vdruta            #+#    #+#             */
-/*   Updated: 2015/11/06 12:57:53 by vdruta           ###   ########.fr       */
+/*   Updated: 2015/11/06 15:08:09 by vdruta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h> //printf
-/*
-   char	*ft_read(int fd, char **line)
-   {
-   int		ret;
-   char	*buffs;
 
-   if (!(buffs = (char*)malloc(sizeof(*buffs) * (BUFF_SIZE + 1))))
-   return (NULL);
-   ret = read(fd, buffs, BUFF_SIZE);
-   buffs[ret] = '\0';
-   return (buffs);
-   if (ret < 0)
-//return (-1);
-if (ret == 0)
+int		read_to_stock(int const fd, char **stock)
 {
-//return (0);
-//get_next_line(fd, buffs);
-}
-//else
-//get_next_line(fd, buffs);
-}
-*/
-/*
-   int		get_next_line(int const fd, char **line)
-   {
-   static char	*stock;
-   char		*pos;
-   int			ret;
-
-   if (fd == -1)	
-   return (-1);
-   if (BUFF_SIZE < 1)
-   return (-1);
-   stock = (char*)malloc(sizeof(*stock) * (BUFF_SIZE + 1));
-   if (!stock)
-   return (-1);
-   pos = ft_strchr(stock, '\n');
-   while (pos == NULL)
-   {
-   ret = read(fd, stock, BUFF_SIZE);
-
-   }
- *line = ft_strsub(stock, 0, pos - stock);
- pos = ft_strdup(pos + 1);
- free (stock);
- stock = pos;
- return (1);
- }
- */
-
-int	read_from_fd_into_stock(int const fd, char **stock)
-{
-	static char	buff[BUFF_SIZE+1] = { '\n' };
-	int			read_bytes;
+	char	buff[BUFF_SIZE + 1];
+	int			ret;
 	char        *nstr;
 
-	read_bytes = read(fd, buff, BUFF_SIZE);
-	if (read_bytes > 0)
+	ret = read(fd, buff, BUFF_SIZE);
+	if (ret > 0)
 	{
-		buff[read_bytes] = '\0';
+		buff[ret] = '\0';
 		nstr = ft_strjoin(*stock, buff);
 		if (!nstr)
 			return (-1);
 		free(*stock);
 		*stock = nstr;
 	}
-	return (read_bytes);
+	return (ret);
 }
 
-int get_next_line(int const fd, char ** line)
+int		get_next_line(int const fd, char ** line)
 {
 	static char	*stock = NULL;
-	char		*endl_index;
+	char		*bn;
 	int         ret;
 
 	if (!stock && (stock = (char *)malloc(sizeof(*stock))) == NULL)
 		return (-1);
-	endl_index = ft_strchr(stock, '\n');
-	while (endl_index == NULL)
+	bn = ft_strchr(stock, '\n');
+	while (bn == NULL)
 	{
-		ret = read_from_fd_into_stock(fd, &stock);
+		ret = read_to_stock(fd, &stock);
 		if (ret == 0)
 		{
-			if ( (endl_index = ft_strchr(stock, '\0')) == stock )
+			if (ft_strlen((bn = ft_strchr(stock, '\0'))) == ft_strlen(stock))
 				return (0);
-		} else if (ret < 0)
+		}
+		else if (ret < 0)
 			return (-1);
 		else
-			endl_index = ft_strchr(stock, '\n');
+			bn = ft_strchr(stock, '\n');
 	}
-	*line = ft_strsub(stock, 0, endl_index - stock);
+	*line = ft_strsub(stock, 0, ft_strlen(stock) - ft_strlen(bn));
 	if (!*line)
 		return (-1);
-	//endl_index = ft_strdup(endl_index + 1);
-	//free(stock);
-	//stock = endl_index;
-	stock = ft_strdup(endl_index + 1);
-	//free(stock);
-	//stock = endl_index;
+	stock = ft_strdup(bn + 1);
 	return (1);
 }
 
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/03 13:09:06 by vdruta            #+#    #+#             */
-/*   Updated: 2015/11/06 12:48:44 by vdruta           ###   ########.fr       */
+/*   Updated: 2015/11/06 15:08:12 by vdruta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,5 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
 #endif
