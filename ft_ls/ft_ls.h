f009404c63e8cba1dd62424b53f937e2ed9e4151
Vasile-ovidiu DRUTA <vdruta@e1p16.cluj.42.fr>
2015-11-10 18:57:23 +0100

    ft_ls

---
+++ ft_ls/ft_ls.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/10 17:56:32 by vdruta            #+#    #+#             */
+/*   Updated: 2015/11/10 18:56:58 by vdruta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+#endif
+/*
+ * ◦ write
+ * ◦ opendir
+ * ◦ readdir
+ * ◦ closedir
+ * ◦ stat
+ * ◦ lstat
+ * ◦ getpwuid
+ * ◦ getgrgid
+ * ◦ listxattr
+ * ◦ getxattr
+ * ◦ time
+ * ◦ ctime
+ * ◦ readlink
+ * ◦ malloc
+ * ◦ free
+ * ◦ perror
+ * ◦ strerror
+ * ◦ exit
+ * */
---
+++ ft_ls/libft/ft_lstnew.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/29 17:55:11 by vdruta            #+#    #+#             */
+/*   Updated: 2015/10/29 18:41:54 by vdruta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list *new;
+
+	if (!(new = (t_list*)malloc(sizeof(*new) * (content_size))))
+		return (NULL);
+	if (content)
+	{
+		new->content = (void *)content;
+		new->content_size = content_size;
+		new->next = NULL;
+	}
+	else
+	{
+		new->content = NULL;
+		new->content_size = 0;
+		new->next = NULL;
+	}
+	return (new);
+}
