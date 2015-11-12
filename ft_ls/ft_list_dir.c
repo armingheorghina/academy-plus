0d571e43d12751afbbf4d17d2a6fca23b367da52
Vasile-ovidiu DRUTA <vdruta@e1p30.cluj.42.fr>
2015-11-12 16:07:21 +0100

    -a flag done

--- ft_ls/ft_list_dir.c
+++ ft_ls/ft_list_dir.c
@@ -6,7 +6,7 @@
 /*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/11 18:16:49 by vdruta            #+#    #+#             */
-/*   Updated: 2015/11/12 11:52:17 by vdruta           ###   ########.fr       */
+/*   Updated: 2015/11/12 16:06:48 by vdruta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,29 @@ int		main(int argc, char **argv)
 	i = 1;
 	while (i < argc)
 	{
+		if (ft_strcmp(argv[i], "-a") == 0)
+			i++;
+		if (argv[i] == NULL) //if nothing is givven after flag;
+			argv[i] = ".";
 		dirp = opendir(argv[i]);
 		if (dirp != NULL)
 		{
 			while ((dp = readdir(dirp)) != NULL)
+			{
 				ft_putendl(dp->d_name);
+				if (dp == NULL)
+					perror("readdir error");
+			}
 			close_dir = closedir(dirp);
 			if (close_dir == -1)
-				perror("no close");
+				perror("closedir error");
 		}
 		else if (dirp == NULL)
 		{
 			ft_putstr(argv[0]);
 			ft_putstr(": ");
-			//ft_putchar(':');
-			//ft_putchar(' ');
 			perror(argv[i]);
 		}
-
 		i++;
 	}
 	return (0);
