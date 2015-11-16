12dbb06214bfb881773d2806b28535715c6cce6c
Vasile-ovidiu DRUTA <vdruta@e1p16.cluj.42.fr>
2015-11-16 17:13:53 +0100

    ls -l

--- ft_ls/ft_inspect_file.c
+++ ft_ls/ft_inspect_file.c
@@ -6,7 +6,7 @@
 /*   By: vdruta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/13 23:26:11 by vdruta            #+#    #+#             */
-/*   Updated: 2015/11/16 15:46:03 by vdruta           ###   ########.fr       */
+/*   Updated: 2015/11/16 17:13:28 by vdruta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -262,21 +262,33 @@ void	ft_putmode(mode_t mode)
 	ft_putchar(' ');
 }
 
-void	ft_putlist_lsa(t_ls_list *start)
+void	ft_putlist_lsa(t_ls_list **start)
 {
-	while (start)
+	t_ls_list *start2;
+
+	start2 = *start;
+	while (start2)
 	{
-		if (!ft_ishidden(start->name))		
+		if (ft_ishidden(start2->name))
 		{
-			ft_putmode(start->mode);
-			ft_putnumberofhardlinks((int)start->nlink, start->biggest_nlink_len);
-			ft_putuid_name(getpwuid(start->uid), start->biggest_uid_len);
-			ft_putgid_name(getgrgid(start->gid), start->biggest_gid_len);
-			ft_putbytes((int)start->bytes_size, start->biggest_size_len);
-			ft_puttime(ctime(&start->mtime));
-			ft_putendl(start->name);
+			*start = start2->next;
+			free(start2);
 		}
-		start = start->next;
+		start2 = start2->next;
+	}
+	while (*start)
+	{
+	//	if (!ft_ishidden(*start->name))		
+	//	{
+			ft_putmode((*start)->mode);
+			ft_putnumberofhardlinks((int)(*start)->nlink, (*start)->biggest_nlink_len);
+			ft_putuid_name(getpwuid((*start)->uid), (*start)->biggest_uid_len);
+			ft_putgid_name(getgrgid((*start)->gid), (*start)->biggest_gid_len);
+			ft_putbytes((int)(*start)->bytes_size, (*start)->biggest_size_len);
+			ft_puttime(ctime((&((*start)->mtime))));
+			ft_putendl((*start)->name);
+	//	}
+		(*start) = (*start)->next;
 	}
 }
 
@@ -289,10 +301,10 @@ int		ft_isflag(int i, char *flag, char **argv)
 
 int		main(int argc, char **argv)
 {
-	DIR			*dirp;
-	struct dirent		*dp;
-	int			close_dir;
-	int			i;
+	DIR				*dirp;
+	struct dirent	*dp;
+	int				close_dir;
+	int				i;
 	t_ls_list		*start;
 	struct stat		*buf;
 	//int			stat_return;
@@ -329,7 +341,7 @@ int		main(int argc, char **argv)
 			}
 			ft_push_bsl_bnl_to_list(start);
 			ft_push_buidl_bgidl_to_list(start);
-			ft_putlist_lsa(start);
+			ft_putlist_lsa(&start);
 			// *TODO free list : use ft_lstdel(start, f_free_str)
 			if (argc > 3 && i != argc - 1) // && ft_isflag(argc - 1, "-a", argv))
 				ft_putchar('\n');
