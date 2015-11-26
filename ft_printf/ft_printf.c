263e3954c442778acecee4f7b453c2764a27a11a
vdruta1 <mrovidius@yahoo.com>
2015-11-26 22:54:46 +0000

    s working

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -16,14 +16,15 @@ int		ft_printf(const char *format, ...)
 {
 	va_list ap;
 	char *s;
+	char *st;
 
 	va_start(ap, format);
-	
+	while ((st = ft_strchr(format, 's')) != NULL)
+	{	
 		s = va_arg(ap, char *);
 		ft_putendl(s);
-		s = va_arg(ap, char *);
-		ft_putendl(s);
-
+		format = ft_strdup(st + 1);
+	}
 	va_end(ap);
 	return (0);
 }
