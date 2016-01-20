f5adcafb5c62c1a4366a1034084f8c522db8222e
vdruta1 <mrovidius@yahoo.com>
2016-01-20 08:52:48 +0000

    working with partialy sorted lists

--- push_swap/ft_extra.c
+++ push_swap/ft_extra.c
@@ -25,8 +25,11 @@ int		ft_nbr_of_sorted_is_bigger_than_unsorted(int *l_a, int aitems)
 	{
 		if (l_a[i] == sorted[i])
 			ok++;
+		else
+			break;
 		i++;
 	}
+	free(sorted);
 	if (ok > i - ok)
 		return (1);
 	return (0);
@@ -50,11 +53,50 @@ int		ft_biggest_is_first(int *l_a, int aitems)
 	return (0);
 }
 
+int		ft_count_unsorted(int *l_a, int aitems)
+{
+	int i;
+	int ok;
+	int *sorted;
+
+	i = 0;
+	ok = 0;
+	sorted = ft_bubble_sort(l_a, aitems);
+	while (i < aitems)
+	{
+		if (l_a[i] == sorted[i])
+			ok++;
+		else
+			break;
+		i++;
+	}
+	free(sorted);
+	return (aitems - ok);
+}
+
+int	ft_other_than_first_two_list_is_sorted(int *l_a, int aitems)
+{
+	int i;
+
+	i = 2;
+	while (i < aitems - 1)
+	{
+		if (l_a[i] > l_a[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	ft_solve_case_special(int *l_a, int *aitems, t_pslist **ops, int *l_b, int *bitems)
 {
 	int items;
+	int unsorted;
+	int count;
 
 	items = *aitems;
+	unsorted = ft_count_unsorted(l_a, *aitems);
+	count = 0;
 	while (!ft_l_a_is_complete_and_sorted(l_a, *aitems, items))
 	{
 		while (!ft_l_a_is_sorted(l_a, *aitems))
@@ -63,18 +105,40 @@ void	ft_solve_case_special(int *l_a, int *aitems, t_pslist **ops, int *l_b, int 
 			{
 				ft_rotate_last_to_start(l_a, *aitems);
 				ft_add_to_list(ops, 9);
+				count++;
 			}
-			ft_swap(l_a, *aitems);
-			ft_add_to_list(ops, 1);
-			ft_rotate_first_to_end(l_a, *aitems);
-			ft_add_to_list(ops, 6);
+			if (unsorted == count && l_a[0] > l_a[1] && ft_other_than_first_two_list_is_sorted(l_a, *aitems))
+			{
+				ft_swap(l_a, *aitems);
+				ft_add_to_list(ops, 1);
+				ft_rotate_first_to_end(l_a, *aitems);
+				ft_add_to_list(ops, 6);
+				ft_rotate_first_to_end(l_a, *aitems);
+				ft_add_to_list(ops, 6);
+			}
+		//	else if (unsorted == count && l_a[0] < l_a[1] && ft_other_than_first_two_list_is_sorted(l_a, *aitems))
+		//	{
+		//		ft_rotate_first_to_end(l_a, *aitems);
+		//		ft_add_to_list(ops, 6);
+		//		ft_rotate_first_to_end(l_a, *aitems);
+		//		ft_add_to_list(ops, 6);
+		//	}
+			else
+			{
+				ft_push(l_b, bitems, l_a, aitems);
+				ft_add_to_list(ops, 5);
+				count--;
+				unsorted--;
+			}
+		}
+		if (!ft_l_a_is_complete_and_sorted(l_a, *aitems, items))
+		{
+			ft_push(l_a, aitems, l_b, bitems);
+			ft_add_to_list(ops, 4);
 			ft_rotate_first_to_end(l_a, *aitems);
 			ft_add_to_list(ops, 6);
 		}
-
 	}
-	(*bitems)++;
-	l_b[0] = 1;
 
 
 	/*
@@ -88,5 +152,5 @@ void	ft_solve_case_special(int *l_a, int *aitems, t_pslist **ops, int *l_b, int 
 	   ft_add_to_list(ops, 6);
 	   ft_rotate_first_to_end(l_a, aitems);
 	   ft_add_to_list(ops, 6);
-	   */
+	 */
 }
