c2d8b7377a27be148db45019f286ecd7a1fe2c33
Vasile-ovidiu DRUTA <vdruta@e1p1.cluj.42.fr>
2016-02-04 17:15:38 +0100

    added rotate for perspective

--- fdf/ft_project_perspective_matrix.c
+++ fdf/ft_project_perspective_matrix.c
@@ -6,11 +6,26 @@
 /*   By: vdruta <vdruta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 14:58:01 by vdruta            #+#    #+#             */
-/*   Updated: 2016/02/03 17:13:36 by vdruta           ###   ########.fr       */
+/*   Updated: 2016/02/04 17:15:05 by vdruta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	ft_rotate_45(t_point *point, float radians)
+{
+	float		c;
+	float		s;
+	float		xp;
+	float		yp;
+
+	c = cos(radians);
+	s = sin(radians);
+	xp = point->y * c - point->z * s;
+	yp = point->y * s + point->z * c;
+	point->y = xp;
+	point->z = yp;
+}
 
 int		ft_give_me_a_number(void)
 {
@@ -35,7 +50,8 @@ t_point	**ft_project_perspective_matrix(t_env *m)
 		while (j < m->columns)
 		{
 			if (ft_give_me_a_number() <= m->rows * m->columns)
-				m->map3[i][j].z += m->rows / 2 + i;
+				ft_rotate_45(&m->map3[i][j], 0.785);
+				//m->map3[i][j].z += m->rows / 2 + i;
 			matrix[i][j].x = WIDTH / 2 + m->spacing * (m->map3[i][j].x + (m->map3[i][j].z * m->map3[i][j].x) / (m->eye.z - m->map3[i][j].z));
 			matrix[i][j].y = HEIGHT / 2 + m->spacing * (m->map3[i][j].y + (m->map3[i][j].z * m->map3[i][j].y) / (m->eye.z - m->map3[i][j].z));
 			matrix[i][j].z = m->map3[i][j].z;
